5ccccb0870f5cbb6c654f7df9ef7c7bfc35b5e00
Tomasz Łukaszyk <el.moue@gmail.com>
2019-01-10 18:38:00 +0100

    Add product class

---
+++ src/main/java/com/codecool/Product.java
@@ -0,0 +1,35 @@
+package com.codecool;
+
+import java.util.Objects;
+
+public class Product {
+
+    private String name;
+    private float price;
+
+    public Product(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
