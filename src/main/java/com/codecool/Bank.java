10b272888174f1b9de16a48cd5d32a7f838988ef
Tomasz Łukaszyk <el.moue@gmail.com>
2019-01-10 19:37:56 +0100

    Implement stockUp method

--- src/main/java/com/codecool/Bank.java
+++ src/main/java/com/codecool/Bank.java
@@ -1,19 +1,31 @@
 package com.codecool;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Bank {
 
     private Map<Coin, Integer> coins = new HashMap<>();
+    private List<Coin> availableCoins = Arrays.asList(Coin.NICKEL, Coin.DIME, Coin.QUARTER);
 
     public Bank() {
-        coins.put(Coin.NICKEL, 0);
-        coins.put(Coin.DIME, 0);
-        coins.put(Coin.QUARTER, 0);
+        availableCoins.forEach(coin -> {
+            coins.put(coin, 0);
+        });
     }
 
-    public void stockUp(Coin coin) {
+    public Map<Coin, Integer> getCoins() {
+        return coins;
+    }
 
+    public void stockUp(Coin coin, int amount) {
+
+        if (!availableCoins.contains(coin)) {
+            return;
+        }
+
+        coins.put(coin, amount);
     }
 }
