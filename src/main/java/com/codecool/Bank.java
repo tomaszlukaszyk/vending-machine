82dee923e6e6fc68bb2ce0c0ffa09a483d99ccd0
Tomasz Łukaszyk <el.moue@gmail.com>
2019-01-10 22:42:21 +0100

    Add makeChange method

--- src/main/java/com/codecool/Bank.java
+++ src/main/java/com/codecool/Bank.java
@@ -1,7 +1,6 @@
 package com.codecool;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Bank {
 
@@ -15,9 +14,7 @@ public class Bank {
         validCoins.put(Coin.DIME, 0.1f);
         validCoins.put(Coin.QUARTER, 0.25f);
 
-        validCoins.keySet().forEach(coin -> {
-            availableCoins.put(coin, 0);
-        });
+        validCoins.keySet().forEach(coin -> availableCoins.put(coin, 0));
     }
 
     public boolean isCoinValid(Coin coin) {
@@ -39,5 +36,38 @@ public class Bank {
         }
 
         availableCoins.put(coin, amount);
+    }
+
+    public List<Coin> makeChange(float amount) {
+        List<Map.Entry<Coin, Float>> denominations = new DenominationsSorter().getSortedDenominations(validCoins);
+        List<Coin> change = new ArrayList<>();
+
+        for (Map.Entry<Coin, Float> denomination: denominations) {
+
+            int count = (int)(amount / denomination.getValue());
+            addToChange(change, denomination, count);
+            amount = amount % denomination.getValue();
+            amount = Math.round(amount * 100.0f) / 100.0f;
+        }
+
+        return change;
+    }
+
+    private class DenominationsSorter {
+
+        List<Map.Entry<Coin, Float>> getSortedDenominations(Map<Coin, Float> map) {
+
+            List<Map.Entry<Coin, Float>> sortedEntries = new ArrayList<>(map.entrySet());
+            sortedEntries.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
+
+            return sortedEntries;
+        }
+    }
+
+    private void addToChange(List<Coin> change, Map.Entry<Coin, Float> denomination, int count) {
+
+        for (int i = 0; i < count; i++) {
+            change.add(denomination.getKey());
+        }
     }
 }
