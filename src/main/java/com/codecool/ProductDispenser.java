617de840659f80d6dda77563742f3572bce0b5bb
Tomasz Łukaszyk <el.moue@gmail.com>
2019-01-10 18:42:49 +0100

    Add ProductDispenser class

---
+++ src/main/java/com/codecool/ProductDispenser.java
@@ -0,0 +1,18 @@
+package com.codecool;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ProductDispenser {
+
+    private Map<Product, Integer> products = new HashMap<>();
+
+    public void addProduct(Product product) {
+    }
+
+    public Set<Product> getAvailableProducts() {
+        return null;
+    }
+
+}
