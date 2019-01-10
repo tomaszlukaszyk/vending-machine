73288e39fc3c68d18a6aeaee9c1f3801c2884a5b
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2019-01-10 14:49:45 +0100

    Change acceptCoin to calculate funds

--- src/main/java/com/codecool/VendingMachine.java
+++ src/main/java/com/codecool/VendingMachine.java
@@ -1,20 +1,29 @@
 package com.codecool;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class VendingMachine {
 
-    private final List<Coin> validCoins = Arrays.asList(Coin.NICKEL, Coin.DIME, Coin.QUARTER);
+    private final Map<Coin, Float> validCoins;
 
     private List<Coin> acceptedCoins = new ArrayList<>();
     private List<Coin> coinReturn = new ArrayList<>();
     private float clientFunds = 0f;
 
+    public VendingMachine() {
+        validCoins = new HashMap<>();
+        validCoins.put(Coin.NICKEL, 0.05f);
+        validCoins.put(Coin.DIME, 0.1f);
+        validCoins.put(Coin.QUARTER, 0.25f);
+    }
+
     public void acceptCoin(Coin coin) {
-        if (validCoins.contains(coin)) {
+        if (validCoins.containsKey(coin)) {
             acceptedCoins.add(coin);
+            clientFunds += validCoins.get(coin);
         } else {
             coinReturn.add(coin);
         }
